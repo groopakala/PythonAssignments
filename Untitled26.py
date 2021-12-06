b2d01f38805a77ccf219982651343986a42fd65c
groopakala <95517628+groopakala@users.noreply.github.com>
2021-12-06 10:39:37 -0500

    Python Assignment-1

---
+++ Untitled26.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+1.  * is an expression
+    'hello' is a String
+    -87.8  is a Value
+    - / * +  is an expression. Expression is collection of operands and operators
+    6 is a Value
+    
+    
+2.  Difference between string and variable. 
+      String is a sequence of characters.  Ex: "This is Python Programming"
+        
+      Vairable is you can store value in a varaible. Variables is reserved memory location
+              to store values . Ex: a= 20, b=30, str="Mystring"  
+            
+3. DAta types in Python:
+    
+     String, int, bool.  Ex: str= "Python",  a=5, b='TRUE'    str is STring datatype, a is int data type
+                              b is Boolean
+            
+            
+
+4. Expresions are combination of values, variables, operators, and calls to functions.Expressions need
+    to be evaluated.If you ask to print an expression the interpretor evaluates the expression and 
+    displays the result.
+    
+    
+    
+5.Difference between expression and statement:
+     Expresions are combination of values, variables, operators, and calls to functions.Expressions need
+    to be evaluated.
+    
+    Statements is an instruction that python interperetor can execute.
+    
+    bacon =22
+    bacon+1
+    
+    
+bacon=22
+bacon+1
+    
+    
+
+
+# In[7]:
+
+
+6. bacon=22
+bacon+1
+
+
+# In[8]:
+
+
+7. 'spam' + 'spamspam'
+'spam' * 3
+
+
+# In[ ]:
+
+
+8. Eggs is a valid vaiable name   while 100 is not a valid varaible name. In variable you can assign some values
+and store in memory locations whereas 100 is a value , it needs a variable to assign and store in the memory
+
+
+9.   Three functions to get integer, floating point and string are:
+    
+      int()
+        str()
+        float()
+        
+        
+10.  "I have eaten" +  99 +  "burritoes"   will give error since you cannot concatinate strring with integer.
+
+     the correct output should be
+    "I have eaten " + "99"  + "burritoes"
+    
+    
+    
+
